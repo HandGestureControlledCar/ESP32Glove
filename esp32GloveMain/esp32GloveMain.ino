885e7aad85b6863b9a923db808129903bcd5166b
omarelfouly <omarelfouly@aucegypt.edu>
2024-12-04 14:48:33 +0200

    flex reading

--- esp32GloveMain/esp32GloveMain.ino
+++ esp32GloveMain/esp32GloveMain.ino
@@ -1,16 +1,24 @@
-const int potPin = 34;
+const int potPin1 = 34;
+const int potPin2 = 35;
 
-// variable for storing the potentiometer value
-int potValue = 0;
+int potValue1 = 0, potValue2 = 0;
+// float vout;
 
 void setup() {
   Serial.begin(115200);
   delay(1000);
+  pinMode(potPin1, INPUT);
+  pinMode(potPin2, INPUT);
 }
 
 void loop() {
   // Reading potentiometer value
-  potValue = analogRead(potPin);
-  Serial.println(potValue);
-  delay(100);
+  potValue1 = analogRead(potPin1);
+  potValue2 = analogRead(potPin2);
+  //vout = (potValue * 5.0) / 1.0;
+  Serial.print("Flex 1: ");
+  Serial.println(potValue1);
+  Serial.print("Flex 2: ");
+  Serial.println(potValue2);
+  delay(500);
 }
